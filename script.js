b8168a0bdc2a343cb51550740657516bb80782dd
Safayat Khan Rohan <safayatkhanrohan@gmail.com>
2024-11-06 00:32:58 +0600

    chore: Update URLs and improve login form in index.html

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 
 
-const inputFileds = document.querySelectorAll("input");
+const toggler = document.querySelector('[toggle="sidebar-toggler"]');
+const sidebar = document.querySelector(".sidebar");
 
-inputFileds.forEach((input) => {
-  const label = input.nextElementSibling;
+toggler.onclick = () => {
+  const windowWidth = window.innerWidth;
+  if (windowWidth > 768) {
+    sidebar.classList.toggle("collapsed");
+  } else {
+    const sidebarMenu = sidebar.querySelector(".sidebar-menu");
+    sidebarMenu.classList.toggle("active");
+  }
+}
 
-  input.addEventListener("input", function () {
-    if (input.value.trim() !== "") {
-      label.classList.add("typed");
-    } else {
-      label.classList.remove("typed");
-    }
-  });
+// take the user to the dashboard page
 
-});
-
-
+document.getElementById("login-form").onsubmit = e => {
+  e.preventDefault();
+  window.location.href = "dashboard.html";
+}
